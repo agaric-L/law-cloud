a5584ccd55a79b762ddee9ed9326d022b828592f
Cong Li <FFB6F1EC@gmail.com>
2025-07-16 09:57:04 +0800

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -10,10 +10,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-
 )
+
 app.include_router(router)
 
